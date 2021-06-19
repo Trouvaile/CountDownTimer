34d7525e27b99d29a2b045af4c7927f8f1088e64
unknown <shioryuu0302@gmail.com>
2021-06-20 00:22:07 +0700

    Count down complete

--- main.js
+++ main.js
@@ -5,30 +5,41 @@ const min = document.getElementById("minutes")
 const sec = document.getElementById("seconds")
 var modal = document.getElementById("myModal")
 
-function CalTime(){
-    let targetTime = new Date(time.value)
-    let currTime = new Date()
-
-    let totalsec = (targetTime - currTime) / 1000
-    if (totalsec <= 0){
-        let modalBody = document.getElementById("err-content")
-        modalBody.innerHTML = "TIME ERROR! CHOOSE ANOTHER DAY"
-        modal.style.display = "block";
-        let span = document.getElementsByClassName("close")[0];
-        span.onclick = function() {
-            modal.style.display = "none";
+function CalTime() {
+    const targetTime = new Date(time.value)
+    let run = setInterval(function () {   
+        let currTime = new Date()
+        let totalsec = (targetTime - currTime) / 1000
+        if (totalsec <= 0) {
+            showErr()
+            clearInterval(run)
+        } else {
+            let d = Math.floor(totalsec / 3600 / 24)
+            let h = Math.floor(totalsec / 3600) % 24
+            let m = Math.floor(totalsec / 60) % 60
+            let s = Math.floor(totalsec) % 60
+            console.log(d,h,m,s)
+            day.innerHTML = d
+            hour.innerHTML = h
+            min.innerHTML = m
+            sec.innerHTML = s
         }
-    } else{
-        let d = Math.floor(totalsec / 3600 / 24)
-        let h = Math.floor(totalsec / 3600) % 24
-        let m = Math.floor(totalsec / 60) % 60
-        let s = Math.floor(totalsec) % 60
-        console.log(d,h,m,s)
-    }
+    }, 1000)
 }
- 
-window.onclick = function(event) {
-  if (event.target == modal) {
-    modal.style.display = "none";
-  }
+
+function showErr() {
+    let modalBody = document.getElementById("err-content")
+    modalBody.innerHTML = "TIME ERROR! CHOOSE ANOTHER DAY"
+    modal.style.display = "block";
+    let span = document.getElementsByClassName("close")[0];
+    span.onclick = function () {
+        modal.style.display = "none";
+    }
+    time.value = ""
+}
+
+window.onclick = function (event) {
+    if (event.target == modal) {
+        modal.style.display = "none";
+    }
 }
