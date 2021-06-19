ed6dd7f7384d899a80adf4ad940c0779e843621c
unknown <shioryuu0302@gmail.com>
2021-06-20 00:02:06 +0700

    add modal box error

--- main.js
+++ main.js
@@ -3,6 +3,7 @@ const day = document.getElementById("days")
 const hour = document.getElementById("hours")
 const min = document.getElementById("minutes")
 const sec = document.getElementById("seconds")
+var modal = document.getElementById("myModal")
 
 function CalTime(){
     let targetTime = new Date(time.value)
@@ -10,7 +11,13 @@ function CalTime(){
 
     let totalsec = (targetTime - currTime) / 1000
     if (totalsec <= 0){
-        console.log("Time err")
+        let modalBody = document.getElementById("err-content")
+        modalBody.innerHTML = "TIME ERROR! CHOOSE ANOTHER DAY"
+        modal.style.display = "block";
+        let span = document.getElementsByClassName("close")[0];
+        span.onclick = function() {
+            modal.style.display = "none";
+        }
     } else{
         let d = Math.floor(totalsec / 3600 / 24)
         let h = Math.floor(totalsec / 3600) % 24
@@ -18,4 +25,10 @@ function CalTime(){
         let s = Math.floor(totalsec) % 60
         console.log(d,h,m,s)
     }
+}
+ 
+window.onclick = function(event) {
+  if (event.target == modal) {
+    modal.style.display = "none";
+  }
 }
